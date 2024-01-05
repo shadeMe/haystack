42bb8916a1c19991a994addda47750ac98a58107
shadeMe <shadeMe@users.noreply.github.com>
2024-01-05 13:27:49 +0100

    feat: Add `NamedEntityExtractor`component
    This component accepts a list of `Document`s which it annotates with named entities. The annotations are stored in the `meta` dictionary of each `Document` under a specific key.
    
    The component currently support two backends for the annotation models: Hugging Face `transformers` and spaCy.

--- e2e/conftest.py
+++ e2e/conftest.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+from importlib.util import find_spec
 
 import pytest
 from haystack.testing.test_utils import set_all_seeds
+
+from .pipelines.test_named_entity_extractor import SPACY_TEST_MODEL_NAME
+
 
 set_all_seeds(0)
 
@@ -9,3 +13,20 @@ set_all_seeds(0)
 @pytest.fixture
 def samples_path():
     return Path(__file__).parent / "samples"
+
+
+def pytest_sessionstart(session):
+    """
+    Called after the Session object has been created and
+    before performing collection and entering the run test loop.
+    """
+    # Set up additional dependencies
+
+    # NamedEntityExtractor
+    import spacy
+
+    # The download function doesn't check it see if the model
+    # is already installed before downloading the remote asset.
+    # We can avoid this behaviour by checking its presence on our end.
+    if find_spec(SPACY_TEST_MODEL_NAME) is None:
+        spacy.cli.download(SPACY_TEST_MODEL_NAME)
